69abee48390233ae9de643724253a1ae919be491
Dmitriy <divideby@yandex.ru>
2017-10-25 19:38:34 +0800

    first web3 function

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const web3 = new Web3();
 
 web3.setProvider(new web3.providers.HttpProvider("http://localhost:8545"));
 
+console.log(web3.eth.coinbase);
+
 app.get("/", function(req, res) {
   res.send("Hello");
 });
